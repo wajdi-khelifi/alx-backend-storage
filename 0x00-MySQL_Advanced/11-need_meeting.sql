f75b7b2d34c0bd0be57fb4fa3d30b756760b329d
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-05-09 21:20:56 +0200

    all

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,11 +1,7 @@
--- meetings
-DELIMITER //
+-- Creates a view need_meeting that lists all students that have a score under 80 (strict)
+-- no last_meeting or more than 1 month
 
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
-BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END //
-
-DELIMITER;
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students WHERE score < 80
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
