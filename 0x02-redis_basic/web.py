97e3c2470f8702b19ad65d204525b34a408aff5a
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-05-15 22:29:07 +0200

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,34 @@
 #!/usr/bin/env python3
-""" web """
-import requests
-import redis
+"""
+Implements an expiring web cache and tracker
+"""
+from typing import Callable
 from functools import wraps
+import redis
+import requests
+redis_client = redis.Redis()
 
 
-def track_and_cache(func):
-    """ track / cache """
-    @wraps(func)
-    def wrapper(url):
-        """ rap god """
-        redis_conn = redis.Redis()
-
-        url_count_key = f"count:{url}"
-        redis_conn.incr(url_count_key)
-
-        cached_content = redis_conn.get(url)
-        if cached_content:
-            return cached_content.decode('utf-8')
-
-        response = requests.get(url)
-        page_content = response.text
-
-        redis_conn.setex(url, 10, page_content)
-
-        return page_content
-
+def url_count(method: Callable) -> Callable:
+    """counts how many times an url is accessed"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
     return wrapper
 
 
-@track_and_cache
+@url_count
 def get_page(url: str) -> str:
-    """ page """
-    return requests.get(url).text
+    """get a page and cache value"""
+    response = requests.get(url)
+    return response.text
 
 
 if __name__ == "__main__":
-    url = ("http://slowwly.robertomurray.co.uk/"
-           "delay/1000/url/"
-           "https://www.example.com")
-    print(get_page(url))
+    get_page('http://slowwly.robertomurray.co.uk')
